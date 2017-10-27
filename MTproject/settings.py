4881317360e25d50cca504765a88f64747c6f794
phucphuong <phucphuong1293@gmail.com>
2017-10-27 15:19:07 +0700

    add image processing before save to db

--- MTproject/settings.py
+++ MTproject/settings.py
@@ -76,26 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'MTproject.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'mactip',
-#         'USER': 'phucphuong',
-#         'PASSWORD': '',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
