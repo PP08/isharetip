58028061a7884502218708591f8487b6fd7a4914
phucphuong <phucphuong1293@gmail.com>
2017-10-30 00:18:47 +0700

    update size of profile photo

--- MacTip/pipeline.py
+++ MacTip/pipeline.py
@@ -12,7 +12,7 @@ def new_users_handler(backend, user, response, *args, **kwargs):
             if backend.name == 'facebook':
                 url = "http://graph.facebook.com/%s/picture" \
                       % response["id"]
-                avatar = request('GET', url, params={'type': 'square'})
+                avatar = request('GET', url, params={'type': 'square', 'height': 1024, 'width': 1024})
                 # profile = UserProfile.objects.update_or_create(user=user)
                 # profile.profile_photo.save(slugify(user.username + " social") + '.jpg',
                 #                            ContentFile(avatar.content))
