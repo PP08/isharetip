5297756c95ff949258f650476748c4798861e3ff
Lance Lot <phucphuong1293@gmail.com>
2017-12-07 00:43:28 +0700

    Mod: url to dictionary

--- MacTip/urls.py
+++ MacTip/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='index'),
+    url(r'^$', views.listapps, name='list_apps'),
     url(r'^post/(?P<slug>[\w-]+)/$', views.post_detail, name='post_detail'),
     url(r'^oauth/disconnect/facebook/$', views.logout, name='logout'),
-    url(r'^macapps/$', views.listapps, name='list_apps'),
+    url(r'^macapps/$', views.listapps, name='index'),
     url(r'^macapps/(?P<slug>\S+)/$', views.appdetail, name='app_detail'),
     url(r'^test/$', views.testAutoComplete, name='testautocomplete'),
     url(r'^search/', views.search, name='search'),
